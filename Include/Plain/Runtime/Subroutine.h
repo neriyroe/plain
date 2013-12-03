94d7f6d5f3b2de0d4bfe7df128f08ed533e5d135
Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>
2013-12-03 22:22:17 +0200

    Simplification.

--- Include/Plain/Runtime/Subroutine.h
+++ Include/Plain/Runtime/Subroutine.h
@@ -1,9 +1,9 @@
 /*
  * Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
  * Date     05/09/2013,
- * Revision 11/15/2013,
+ * Revision 12/03/2013,
  *
  * Copyright 2013 Nerijus Ramanauskas.
  */
 
-typedef MOCOSEL_WORD_DOUBLE (*MOCOSEL_SUBROUTINE) (void* MOCOSEL_RESTRICT, struct MOCOSEL_LIST* MOCOSEL_RESTRICT, struct MOCOSEL_SEGMENT* MOCOSEL_RESTRICT, struct MOCOSEL_VALUE* MOCOSEL_RESTRICT);
+typedef MOCOSEL_WORD_DOUBLE (*MOCOSEL_SUBROUTINE) (void*, struct MOCOSEL_LIST*, struct MOCOSEL_SEGMENT*, struct MOCOSEL_VALUE*);
