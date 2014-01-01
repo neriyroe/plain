6fe004f6a7edf8260f94314a2c3e559b5b62b748
Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>
2014-01-02 01:37:28 +0200

    New targets for CMake.

---
+++ Deployment/Darwin/x86/Target.cmake
@@ -0,0 +1,9 @@
+#
+# Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
+# Date     01/01/2014,
+# Revision 01/01/2014,
+#
+# Copyright 2013 Nerijus Ramanauskas.
+#
+
+ADD_DEFINITIONS("-arch i386" "-arch x86_64")
