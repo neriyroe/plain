7beb2b97d6fb0e9fac112f825b213e5648524b59
Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>
2014-01-02 01:56:52 +0200

    Revert "New targets for CMake."
    This reverts commit 6fe004f6a7edf8260f94314a2c3e559b5b62b748.

--- Deployment/Darwin/x86/Target.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
-# Date     01/01/2014,
-# Revision 01/01/2014,
-#
-# Copyright 2013 Nerijus Ramanauskas.
-#
-
-ADD_DEFINITIONS("-arch i386" "-arch x86_64")
--- Deployment/Linux/x86/Target.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
-# Date     01/01/2014,
-# Revision 01/01/2014,
-#
-# Copyright 2013 Nerijus Ramanauskas.
-#
-
-ADD_DEFINITIONS("-m32")
