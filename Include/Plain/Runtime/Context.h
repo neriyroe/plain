90ff2e14dd5a0a3077df3d9a00423a0fbc181069
Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>
2014-01-12 07:55:38 +0200

    Adding missing headers.

---
+++ Include/Plain/Runtime/Context.h
@@ -0,0 +1,9 @@
+/*
+ * Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
+ * Date     01/12/2014,
+ * Revision 01/12/2014,
+ *
+ * Copyright 2013 Nerijus Ramanauskas.
+ */
+
+typedef void* MOCOSEL_CONTEXT;
---
+++ Include/Plain/Runtime/Lookup.h
@@ -0,0 +1,9 @@
+/*
+ * Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
+ * Date     01/12/2014,
+ * Revision 01/12/2014,
+ *
+ * Copyright 2013 Nerijus Ramanauskas.
+ */
+
+typedef struct MOCOSEL_VALUE* (*MOCOSEL_LOOKUP) (MOCOSEL_CONTEXT*, const MOCOSEL_GLYPH*, const struct MOCOSEL_LIST*);
