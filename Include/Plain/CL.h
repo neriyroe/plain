caea620816ff07617a5eb5fcb9db8983c2c81ec0
Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>
2014-06-01 15:40:22 +0300

    Committing new and missing files, supporting ARMv6.

---
+++ Include/Plain/CL.h
@@ -0,0 +1,38 @@
+/*
+ * Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
+ * Date     05/25/2014,
+ * Revision 05/25/2014,
+ *
+ * Copyright 2014 Nerijus Ramanauskas.
+ */
+
+#pragma once
+
+/* C++. */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MOCOSEL_MAXIMUM(LEFT, RIGHT) ((LEFT) > (RIGHT)? (LEFT): (RIGHT))
+#define MOCOSEL_MINIMUM(LEFT, RIGHT) ((LEFT) < (RIGHT)? (LEFT): (RIGHT))
+#define MOCOSEL_PAIR(LEFT, RIGHT, TYPE) struct TYPE {LEFT first; RIGHT second;}
+
+#include "System/Platform.h"
+#include "Shared/Segment.h"
+
+/* Appends <source> to <destination>. Note that <destination> will be reallocated. */
+MOCOSEL_WORD_DOUBLE MOCOSEL_CONCAT(struct MOCOSEL_SEGMENT* MOCOSEL_RESTRICT destination, const struct MOCOSEL_SEGMENT* MOCOSEL_RESTRICT source);
+
+/* Returns 32-bit hash of <data>. */
+MOCOSEL_WORD_DOUBLE MOCOSEL_HASH(const MOCOSEL_BYTE* data, MOCOSEL_WORD_DOUBLE length);
+
+/* Reserves <number> bytes of memory in <segment>. */
+MOCOSEL_WORD_DOUBLE MOCOSEL_RESERVE(MOCOSEL_WORD_DOUBLE number, struct MOCOSEL_SEGMENT* segment);
+
+/* Allocates <destination> bytes of memory. Note that <data> will be reallocated if <source> > 0, released if destination = 0. */
+void* MOCOSEL_RESIZE(void* data, MOCOSEL_WORD_DOUBLE destination, MOCOSEL_WORD_DOUBLE source);
+
+/* C++. */
+#ifdef __cplusplus
+}
+#endif
---
+++ Include/Plain/Framework/Host/Environment.h
@@ -0,0 +1,14 @@
+ /*
+ * Author   Nerijus Ramanauskas <nerijus.ramanauskas@mocosel.org>,
+ * Date     10/12/2013,
+ * Revision 05/25/2014,
+ *
+ * Copyright 2014 Nerijus Ramanauskas.
+ */
+
+struct MOCOSEL_ENVIRONMENT {
+    struct { 
+        const char* pattern; 
+        const char* version;
+    } meta;
+};
