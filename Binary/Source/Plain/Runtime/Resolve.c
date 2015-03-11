4b3e6e8934e01ab37927edaf523d96296326eb07
Nerijus Ramanauskas <nr@mocosel.com>
2015-03-11 22:43:38 +0200

    Cleaning up.

--- Binary/Source/Plain/Runtime/Resolve.c
+++ Binary/Source/Plain/Runtime/Resolve.c
@@ -10,22 +10,25 @@
 #include <Plain/VM.h>
 
 MOCOSEL_WORD_DOUBLE resolve(void* context, void* data, MOCOSEL_WORD_DOUBLE type, struct MOCOSEL_VALUE* value) {
-    if(type == MOCOSEL_TYPE_LIST) {
+    if(type == MOCOSEL_TYPE_LIST) { /* List. */
         MOCOSEL_LIST* node = (MOCOSEL_LIST*)data;
+        /* Make sure it's not empty. */
         if(node->keyword.from != node->keyword.to) {
             MOCOSEL_WORD_DOUBLE index = 0;
             MOCOSEL_WORD_DOUBLE length = MOCOSEL_MEASURE(node);
+            /* Arguments. */
             for(; index < length; index++) {
                 MOCOSEL_VALUE* argument = MOCOSEL_ARGUMENT(node, index);
-                if(argument->type == MOCOSEL_TYPE_BOOLEAN) {
+                /* After substitution. */
+                if(argument->type == MOCOSEL_TYPE_BOOLEAN) { /* Boolean. */
                     printf("Resolving boolean %d.\n", argument->data != NULL);
-                } else if(argument->type == MOCOSEL_TYPE_NIL) {
+                } else if(argument->type == MOCOSEL_TYPE_NIL) { /* Nil. */
                     printf("Resolving nil.\n");
-                } else if(argument->type == MOCOSEL_TYPE_INTEGER) {
+                } else if(argument->type == MOCOSEL_TYPE_INTEGER) { /* Integer. */
                     printf("Resolving integer %d.\n", *(const MOCOSEL_WORD_DOUBLE*)argument->data);
-                } else if(argument->type == MOCOSEL_TYPE_REAL) {
+                } else if(argument->type == MOCOSEL_TYPE_REAL) { /* Real. */
                     printf("Resolving real %f.\n", *(const MOCOSEL_REAL*)argument->data);
-                } else if(argument->type == MOCOSEL_TYPE_STRING) {
+                } else if(argument->type == MOCOSEL_TYPE_STRING) { /* String. */
                     printf("Resolving string %s.\n", (const char*)argument->data);
                 }
             }
