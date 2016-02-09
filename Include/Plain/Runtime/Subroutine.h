d3048d56f7c47e0968b94e7f77fc6a9f1cd24a7d
Moco Project, The <project@mocosel.com>
2016-02-09 16:07:10 +0200

    Update Subroutine.h

--- Include/Plain/Runtime/Subroutine.h
+++ Include/Plain/Runtime/Subroutine.h
@@ -1,9 +1,9 @@
 /*
  * Author   Neriy Roe <nr@mocosel.com>.
  * Date     05/09/2013.
- * Revision 09/01/2015.
+ * Revision 02/09/2016.
  *
- * Copyright 2015 Nerijus Ramanauskas.
+ * Copyright 2016 Nerijus Ramanauskas.
  */
 
-typedef MOCOSEL_WORD_DOUBLE (*MOCOSEL_SUBROUTINE) (void*, void*, MOCOSEL_WORD_DOUBLE, struct MOCOSEL_VALUE*);
+typedef MOCOSEL_WORD_DOUBLE (*MOCOSEL_SUBROUTINE) (void* context, void* data, MOCOSEL_WORD_DOUBLE type, struct MOCOSEL_VALUE* value);
